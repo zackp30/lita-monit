6214e9b182884f14659b23fcbcb8ec9e98dbbca4
Zack Piper <zack.piper123@gmail.com>
2015-10-29 01:55:24 +0000

    Update for Lita 4.

--- lib/lita/handlers/monit.rb
+++ lib/lita/handlers/monit.rb
@@ -3,44 +3,44 @@ require "lita"
 module Lita
   module Handlers
     class Monit < Handler
-    	def self.default_config(config)
-    		config.rooms = :all
-    		config.token = nil
-    	end
+      def self.default_config(config)
+        config.rooms = :all
+        config.token = nil
+      end
 
-    	http.post "/monit", :receive
+      http.post "/monit", :receive
 
-    	def receive(request, response)
+      def receive(request, response)
         return unless token_valid?(request)
         params = build_params(request.params)
-        
-    		message = build_message(params)
-        target = Source.new(nil, Lita.config.handlers.monit.rooms)
+
+        message = build_message(params)
+        target = Source.new(room: Lita.config.handlers.monit.rooms)
         puts target
         robot.send_message(target, message)
-    	end
+      end
 
-    	private
-        def build_params(params)
-          {"message" => "", "service" => "", "description" => ""}.merge!(params)
+      private
+      def build_params(params)
+        {"message" => "", "service" => "", "description" => ""}.merge!(params)
+      end
+
+      def token_valid?(request)
+        token = Lita.config.handlers.monit.token
+        begin
+          token.eql? request.params["token"]
+        rescue
+          false
         end
+      end
 
-        def token_valid?(request)
-          token = Lita.config.handlers.monit.token
-          begin
-            token.eql? request.params["token"]
-          rescue
-            false
-          end
-        end
-
-    	def build_message(params)
+      def build_message(params)
         <<-MSG.chomp
 [Monit Alert] #{params['message']}
 From #{params['service']}
 Description #{params['description']}
         MSG
-    	end
+      end
     end
 
     Lita.register_handler(Monit)
